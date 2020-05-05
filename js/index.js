a0c240248104fea4d2a1705603fd903964001f9f
Ankitanshu Swaroop <59068026+ankitanshu22@users.noreply.github.com>
2020-05-06 05:07:57 +0530

    Fonts Updated, Added Responsive Navbar.

--- js/index.js
+++ js/index.js
@@ -1,44 +1,71 @@
-const mHTML = document.getElementById('text-profile'),
- text = [
-  'Web Developer',
-  'Web Designer',
-  'UI/UX Designer'
- ];
+const mHTML = document.getElementById("text-profile"),
+  text = ["Web Developer", "Web Designer", "UI/UX Designer"];
 let currentText = 0;
 function typeText() {
- if (!text[currentText]) {
-  currentText = 0;
- }
- const currentStr = text[currentText];
- currentStr.split();
- let part = '';
- let currentLetter = 0;
- let int1 = setInterval(() => {
-  if (!currentStr[currentLetter]) {
-   currentText++;
-   setTimeout(() => {
-    deleteText(part);
-   }, 100);
-   clearInterval(int1);
-  } else {
-   part += currentStr[currentLetter++];
-   mHTML.innerHTML = part;
+  if (!text[currentText]) {
+    currentText = 0;
   }
- }, 100);
+  const currentStr = text[currentText];
+  currentStr.split();
+  let part = "";
+  let currentLetter = 0;
+  let int1 = setInterval(() => {
+    if (!currentStr[currentLetter]) {
+      currentText++;
+      setTimeout(() => {
+        deleteText(part);
+      }, 100);
+      clearInterval(int1);
+    } else {
+      part += currentStr[currentLetter++];
+      mHTML.innerHTML = part;
+    }
+  }, 100);
 }
 function deleteText(str) {
- let int = setInterval(() => {
-  if (str.length === 0) {
-   setTimeout(() => {
-    typeText();
-   }, 100);
-   clearInterval(int);
-  } else {
-   str = str.split('');
-   str.pop();
-   str = str.join('');
-   mHTML.innerHTML = str;
-  }
- }, 100);
+  let int = setInterval(() => {
+    if (str.length === 0) {
+      setTimeout(() => {
+        typeText();
+      }, 100);
+      clearInterval(int);
+    } else {
+      str = str.split("");
+      str.pop();
+      str = str.join("");
+      mHTML.innerHTML = str;
+    }
+  }, 100);
 }
 typeText();
+
+// $('ul li').on('click', function () {
+//  $('li').removeClass('active');
+//  $(this).addClass('active');
+// });
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+ const burger2 = document.querySelector(".burger-2");
+ const burger3 = document.querySelector(".burger-3");
+ const burger4 = document.querySelector(".burger-4");
+ const burger5 = document.querySelector(".burger-5");
+  const nav = document.querySelector(".nav-links");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+ burger2.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+ });
+ burger3.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+ });
+ burger4.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+ });
+ burger5.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+ });
+};
+
+navSlide();
